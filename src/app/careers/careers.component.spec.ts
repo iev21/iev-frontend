96ecd623906e7e45dbd322460fdb42e045186c2b
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-25 22:48:03 +0200

    Update careers.component.spec.ts

--- src/app/careers/careers.component.spec.ts
+++ src/app/careers/careers.component.spec.ts
@@ -1,5 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// careers.component.spec.ts
 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CareersComponent } from './careers.component';
 
 describe('CareersComponent', () => {
@@ -11,7 +12,9 @@ describe('CareersComponent', () => {
       declarations: [ CareersComponent ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CareersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
