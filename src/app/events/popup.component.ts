8a7f70a548462b45059e5690413955fc7963468e
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-25 21:19:48 +0200

    Update popup.component.ts

--- src/app/events/popup.component.ts
+++ src/app/events/popup.component.ts
@@ -1,1 +1,10 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./popup.component.scss']
+})
+export class EventsComponent {
+
+}
