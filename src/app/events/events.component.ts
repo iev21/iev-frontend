cbfc1df0b90151dfb083335cc8da0fbd4a501b4e
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-26 20:12:15 +0200

    Add files via upload

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,3 @@
-// events.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,18 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
-  // Define the events property with sample data
-  events: { name: string, date: string, location: string, rsvp: boolean }[] = [
-    { name: 'Small Business Meet Up', date: 'tis 09 march', location: 'Mountain view', rsvp: false },
-    { name: 'City Networking Event', date: 'tis 03 april', location: 'Flat iron district', rsvp: false },
-    { name: 'Sun Roof Party', date: 'tis 06 feb', location: 'West village', rsvp: false }
-  ];
 
-  // Method to handle RSVP submission
-  submitRSVP(event: { name: string, date: string, location: string, rsvp: boolean }): void {
-    // Implement the functionality to handle RSVP submission here
-    // For demonstration, we're just logging the event details
-    console.log('RSVP submitted for:', event);
-  }
 }
-
