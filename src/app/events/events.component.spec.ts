388d5f135237ea6fc959a6d072cbf8a51fd5c6bc
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-28 00:57:05 +0200

    Add files via upload

--- src/app/events/events.component.spec.ts
+++ src/app/events/events.component.spec.ts
@@ -1,47 +1,23 @@
-import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-events',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
-})
-export class EventsComponent {
-  events = [
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    // Add more events as needed
-  ];
-  showRSVPForm = false;
-  selectedEvent: any;
-  rsvpName: string = '';
-  rsvpEmail: string = '';
-  rsvpPhone: string = '';
+import { EventsComponent } from './events.component';
 
-  openModal(event: any) {
-    this.showRSVPForm = true;
-    this.selectedEvent = event;
-  }
+describe('EventsComponent', () => {
+  let component: EventsComponent;
+  let fixture: ComponentFixture<EventsComponent>;
 
-  closeModal() {
-    this.showRSVPForm = false;
-    this.resetForm();
-  }
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EventsComponent ]
+    })
+    .compileComponents();
 
-  resetForm() {
-    this.rsvpName = '';
-    this.rsvpEmail = '';
-    this.rsvpPhone = '';
-  }
+    fixture = TestBed.createComponent(EventsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  submitRSVP() {
-    // Here you can implement logic to handle RSVP submission, e.g., send data to server
-    console.log('RSVP submitted:');
-    console.log('Name:', this.rsvpName);
-    console.log('Email:', this.rsvpEmail);
-    console.log('Phone:', this.rsvpPhone);
-    
-    // After submission, close the modal and reset form
-    this.closeModal();
-  }
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,20 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { EventsComponent } from './events/events.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    EventsComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss']
 })
-export class AppModule { }
+export class EventsComponent {
+
+}
