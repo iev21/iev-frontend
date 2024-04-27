ebd4a56363748825a6cb38eaf02452c951c320c4
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-28 01:02:16 +0200

    Update events.component.spec.ts

--- src/app/events/events.component.spec.ts
+++ src/app/events/events.component.spec.ts
@@ -1,23 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { EventsComponent } from './events.component';
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.scss']
+})
+export class EventsComponent {
+  events = [
+    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
+    // Add more events as needed
+  ];
+  showRSVPForm = false;
+  selectedEvent: any;
+  rsvpName: string = '';
+  rsvpEmail: string = '';
+  rsvpPhone: string = '';
 
-describe('EventsComponent', () => {
-  let component: EventsComponent;
-  let fixture: ComponentFixture<EventsComponent>;
+  openModal(event: any) {
+    this.showRSVPForm = true;
+    this.selectedEvent = event;
+  }
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EventsComponent ]
-    })
-    .compileComponents();
+  closeModal() {
+    this.showRSVPForm = false;
+    this.resetForm();
+  }
 
-    fixture = TestBed.createComponent(EventsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  resetForm() {
+    this.rsvpName = '';
+    this.rsvpEmail = '';
+    this.rsvpPhone = '';
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  submitRSVP() {
+    // Here you can implement logic to handle RSVP submission, e.g., send data to server
+    console.log('RSVP submitted:');
+    console.log('Name:', this.rsvpName);
+    console.log('Email:', this.rsvpEmail);
+    console.log('Phone:', this.rsvpPhone);
+    
+    // After submission, close the modal and reset form
+    this.closeModal();
+  }
+}
