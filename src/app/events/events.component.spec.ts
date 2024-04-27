7b5207bc5612cabbef37bb792da9a8b4055a2e15
iev21 <166929611+iev21@users.noreply.github.com>
2024-04-28 01:10:53 +0200

    Update events.component.spec.ts

--- src/app/events/events.component.spec.ts
+++ src/app/events/events.component.spec.ts
@@ -1,47 +1,21 @@
-import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Make sure you import FormsModule for ngModel
 
-@Component({
-  selector: 'app-events',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+import { AppComponent } from './app.component';
+// Import the EventsComponent correctly
+import { EventsComponent } from './events/events.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // Don't forget to include FormsModule here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class EventsComponent {
-  events = [
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    { title: 'Small Business Meet Up', date: new Date('2024-03-09'), location: 'Mountain View' },
-    // Add more events as needed
-  ];
-  showRSVPForm = false;
-  selectedEvent: any;
-  rsvpName: string = '';
-  rsvpEmail: string = '';
-  rsvpPhone: string = '';
-
-  openModal(event: any) {
-    this.showRSVPForm = true;
-    this.selectedEvent = event;
-  }
-
-  closeModal() {
-    this.showRSVPForm = false;
-    this.resetForm();
-  }
-
-  resetForm() {
-    this.rsvpName = '';
-    this.rsvpEmail = '';
-    this.rsvpPhone = '';
-  }
-
-  submitRSVP() {
-    // Here you can implement logic to handle RSVP submission, e.g., send data to server
-    console.log('RSVP submitted:');
-    console.log('Name:', this.rsvpName);
-    console.log('Email:', this.rsvpEmail);
-    console.log('Phone:', this.rsvpPhone);
-    
-    // After submission, close the modal and reset form
-    this.closeModal();
-  }
-}
+export class AppModule { }
