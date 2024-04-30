c4949dea2372e9f999faf9950cec841d87b3d6e0
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-01 00:09:09 +0200

    Add files via upload

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss'],
-  animations: [
-    trigger('rollIn', [
-      transition(':enter', [
-        style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate('500ms ease-out', style({ transform: 'translateX(0)', opacity: 1 })),
-      ])
-    ])
-  ]
+  styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
-  events = [
-    { title: 'Event 1' },
-    { title: 'Event 2' },
-    { title: 'Event 3' },
-    // Add more events as needed
-  ];
+
 }
