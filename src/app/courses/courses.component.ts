fd701b50bc2f41e55583b3cc2a50cda846a64fc9
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-04 08:51:56 +0200

    Update courses.component.ts

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -12,6 +12,7 @@ export class CoursesComponent implements OnInit {
       //img: 'https://static.wixstatic.com/media/fc7570_edede96df98b4bc5b8529d1846dac713~mv2.jpg/v1/fill/w_410,h_243,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image-place-holder.jpg',
       title: 'Introduction to Electric Vehicles ',
       description: 'Explore the fundamentals of electric vehicles in our introductory course. Engaging lectures and practical demonstrations await. Join us today!',
+      Readmore: 'Hi',
       // price: '$100',
       duration: '1 day'
     },
