909be12cbd30f2a9a22ce014efcdfbdec6b8b93a
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-04 15:04:33 +0200

    Update courses.component.ts

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -17,7 +17,8 @@ export class CoursesComponent implements OnInit {
       duration: '1 day'
     },
     {
-      img: 'https://static.wixstatic.com/media/fc7570_ac1f7e7e38a44bd2ab92fb30b5fa38d7~mv2.jpg/v1/fill/w_410,h_243,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image-place-holder.jpg',
+      img:'../../assets/Introduction_to_battery_technologies.jpeg',
+      //img: 'https://static.wixstatic.com/media/fc7570_ac1f7e7e38a44bd2ab92fb30b5fa38d7~mv2.jpg/v1/fill/w_410,h_243,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image-place-holder.jpg',
       title: 'Introduction to Battery Technolgies',
       description: 'Explore foundational Battery Technologies with our comprehensive course. Gain insights, skills, and practical knowledge essential for todays energy innovation.',
       readmore: 'Hi',
