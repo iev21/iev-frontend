41b40af2b5e4cadfd6098936e12c75df98d6f194
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-04 19:02:46 +0200

    Update courses.component.ts

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CoursesComponent implements OnInit {
   public basicCourseList: Array<any> = [
     {
-      img: '../../assets/Introduction_to_electric_vehicles_01.jpeg',
+      img: '../../assets/Introduction_to_electric_vehicles.jpeg',
       //img: 'https://static.wixstatic.com/media/fc7570_edede96df98b4bc5b8529d1846dac713~mv2.jpg/v1/fill/w_410,h_243,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image-place-holder.jpg',
       title: 'Introduction to Electric Vehicles ',
       description: 'Explore the fundamentals of electric vehicles in our introductory course. Engaging lectures and practical demonstrations await. Join us today!',
