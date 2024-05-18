08ad1687fc1ce2d2f922d4cf855fd02f489a7dd5
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-18 14:45:05 +0200

    Update courses.component.ts

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -16,7 +16,7 @@ export class CoursesComponent implements OnInit {
       //readmore: 'We will send you a course brochure with your registered information after you submit your information to our Contact Us page.',
       //pdfPath:'../../assets/eMobil digital course.pdf',
       // price: '$100',
-      readmore:'../../assets/Course-Content/Introduction_to_Electric_Vehiicles.html',
+      readmore:'../../assets/Course-Content/Introduction_to_Electric_Vehicles.html',
       duration: '1 day'
     },
     {
