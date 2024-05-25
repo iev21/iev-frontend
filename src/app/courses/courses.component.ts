1e92e2308586d5de7cabc33256a26436640522cc
iev21 <166929611+iev21@users.noreply.github.com>
2024-05-25 09:40:36 +0200

    Update courses.component.ts

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -104,7 +104,7 @@ export class CoursesComponent implements OnInit {
   ];
  public advancedCourseList: Array<any> = [
     {
-      img: '../../assets/Advanced Powertrain Simulations_01.png',
+      img: '../../assets/Course-Images/Advanced Powertrain Simulations_02.jpg',
       //img: 'https://static.wixstatic.com/media/fc7570_edede96df98b4bc5b8529d1846dac713~mv2.jpg/v1/fill/w_410,h_243,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image-place-holder.jpg',
       title: 'Advance Course on BEV Powertrain Modeling & Simulations',
       description: 'Explore cutting-edge BEV Powertrain Modeling & Simulations with our Advanced Course, empowering engineers to innovate in electric vehicle technology. Join IEV Technologies for unparalleled expertise!',
